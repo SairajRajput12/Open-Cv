be472f1f27d7fd0c98b294bb85d6ea54a96c1be3
SairajRajput12 <sairajrajput@gmail.com>
2024-02-18 14:59:15 +0530

    Image smoothing and image blurring tutorial

--- Tut_6.py
+++ Tut_6.py
@@ -3,4 +3,19 @@ import numpy as np
 
 print('this is tutorial for cropping an image') 
 
+# Cropping the images: 
+# Now let’s say we wanted to crop naruto’s face then we would need four values namely X1 (lower bound on the x-axis),  X2 (Upper bound on y-axis), Y1 (lower bound on the y-axis) and Y2 (Upper bound on the y-axis). 
+# Syntax: 
+# face_roi = img [Start X : End X, Start Y: End Y]
+
+img = cv2.imread('naruto.jpg',1) 
+face_roi = img[100:270,200:450] 
+cv2.imshow(
+    'I am Batman',
+    face_roi
+)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 
---
+++ Tut_7.py
@@ -0,0 +1,5 @@
+import cv2 
+import numpy as np 
+
+print('This is tutorial for image smoothing and image blurring')
+
